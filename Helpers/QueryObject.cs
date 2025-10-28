1a6ad0e68ec89645389845c832fff2b74657b1ca
sofia <ssarmiento088@gmail.com>
2025-10-27 23:19:53 -0500

    feat: update filters

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -2,9 +2,13 @@ namespace catalogo_service.Helpers
 {
     public class QueryObject
     {
-        public string? Categoria { get; set; }
-        public string? Color { get; set; }
-        public string? Talla { get; set; }
+        public List<string>? Categoria { get; set; }
+        public List<string>? Color { get; set; }
+        public List<string>? Talla { get; set; }
+        public List<string>? Genero { get; set; }
+        public List<string>? Deporte { get; set; }
+        public List<string>? Tipo { get; set; }
+        public List<string>? Coleccion { get; set; }
         public decimal? PrecioMin { get; set; }
         public decimal? PrecioMax { get; set; }
     }
